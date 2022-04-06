5ebdeca2bca540c3372276b1c43b50f130577c12
24003001 <24003001@L815WD220028.edu.cbe.ab.ca>
2022-04-06 13:28:00 -0600

    h

--- C3/src/C2.java
+++ C3/src/C2.java
@@ -1,9 +1,31 @@
+/*
+
+Program: Chapter 3.java          Date: April 6, 2022
+
+
+Author: Mr.Abdalla
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
 
 public class C2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public static void main(String[] args)
+	{
+		
+		int grade = 77;
+		double price = 15.8;
+		String fullName= "Mohammad Mateen";
+		
+		System.out.print("From Mr.Wilson %n Address: Acer Computer 314" );
 
 	}
 
 }
+/* Screen Dump
+
+
+
+ */
