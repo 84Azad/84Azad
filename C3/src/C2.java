a870c5ef73922a1a0c9d127ee12854f41cab73b9
24003001 <24003001@L815WD220028.edu.cbe.ab.ca>
2022-04-07 15:18:17 -0600

    age

--- C3/src/C2.java
+++ C3/src/C2.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /*
 
 Program: Chapter 3.java          Date: April 6, 2022
@@ -15,12 +17,22 @@ public class C2 {
 	public static void main(String[] args)
 	{
 		
-		int grade = 77;
-		double price = 15.8;
-		String fullName= "Mohammad Mateen";
 		
-		System.out.printf("From Mr.Wilson %nAddress: Acer Computer 314%nDate: November 2, 2020%n%n%nTo: Mohammad Mateen"
-				+ "%n%n%nMessage: Help! I'm stuck in a computer!" );
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Hello, what is your name?");
+		String fullName = input.nextLine();
+		System.out.print(fullName);
+		
+		System.out.println(" Hello, what is your age?");
+		int age = input.nextInt();
+		System.out.print("Your name is: "+fullName + " and your age is "  +age);
+	
+		
+		
+		
+		
+		
 
 	}
 
